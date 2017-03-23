1e2ff1a7ed59f1a3e025bf901c7caa5890163fe0
Asia Kane and Sara Schultz <sara@>
2017-03-23 16:06:58 -0700

    add styles to results section in styles.css and add another form with logic in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,16 @@ $(function() {
     var personalityInput = $("#personality").val();
     var perspectiveInput = $("#perspective").val();
     var teamInput = $("#team").val();
+    var homeInput = $("#home").val();
     event.preventDefault();
 
-    if (personalityInput === "Introverted" && perspectiveInput === "Pessimistic" && teamInput === "By myself"){
+    if (personalityInput === "Introverted" && perspectiveInput === "Pessimistic" && teamInput === "By myself" && homeInput === "Home body"){
       $(".name").text(nameInput);
       $("#sloth").show();
-    } else if (personalityInput === "Extroverted" && (perspectiveInput === "Optimistic" || perspectiveInput === "Pessimistic") && teamInput === "By myself"){
+    } else if (personalityInput === "Extroverted" && (perspectiveInput === "Optimistic" || perspectiveInput === "Pessimistic") && teamInput === "By myself" && homeInput === "Go out"){
       $(".name").text(nameInput);
       $("#squirrel").show();
-    } else if (personalityInput === "Extroverted" && perspectiveInput === "Optimistic" && teamInput === "On a team"){
+    } else if (personalityInput === "Extroverted" && perspectiveInput === "Optimistic" && teamInput === "On a team" && homeInput === "Go out"){
       $(".name").text(nameInput);
       $("#monkey").show();
     } else {
