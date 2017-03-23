e130a6ab0d7564c0a6bfe299ee5ec05b7fa1b990
Asia Kane and Sara Schultz <sara@>
2017-03-23 12:00:41 -0700

    add variables for each selection and attach value, and add submit button to form.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(function() {
+  var personalityInput = $("#personality").val();
+  var perspectiveInput = $("#perspective").val();
+  var teamInput = $("#team").val();
+
+  $("#formOne").submit(function(event) {
+    
+
+
+    event.preventDefault();
+  });
+});
