460369e50982bca608d3463e88803aa53829e921
Asia Kane and Sara Schultz <asia@>
2017-03-23 13:31:56 -0700

    add js script to display spirit animal using branching logic based on user input into form

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,16 @@ $(function() {
     var perspectiveInput = $("#perspective").val();
     var teamInput = $("#team").val();
     event.preventDefault();
+
+    if (personalityInput === "Introverted" && perspectiveInput === "Pessimistic" && teamInput === "By myself"){
+      $("#sloth").show();
+    } else if (personalityInput === "Extroverted" && perspectiveInput === "Optimistic" && teamInput === "By myself"){
+      $("#squirrel").show();
+    } else if (personalityInput === "Extroverted" && perspectiveInput === "Optimistic" && teamInput === "On a team"){
+      $("#monkey").show();
+    } else {
+      $("#dog").show();
+    }
   });
+
 });
