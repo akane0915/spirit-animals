e08754483364e515879038fc9015b2d1ee57bfc8
Asia Kane and Sara Schultz <sara@>
2017-03-23 15:53:48 -0700

    add span for each spirit animal to take users name and display in paragraph

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,23 @@
 $(function() {
   $("#formOne").submit(function(event) {
+    var nameInput  = $("#name").val();
     var personalityInput = $("#personality").val();
     var perspectiveInput = $("#perspective").val();
     var teamInput = $("#team").val();
     event.preventDefault();
 
     if (personalityInput === "Introverted" && perspectiveInput === "Pessimistic" && teamInput === "By myself"){
+      $(".name").text(nameInput);
       $("#sloth").show();
-    } else if (personalityInput === "Extroverted" && perspectiveInput === "Optimistic" && teamInput === "By myself"){
+    } else if (personalityInput === "Extroverted" && (perspectiveInput === "Optimistic" || perspectiveInput === "Pessimistic") && teamInput === "By myself"){
+      $(".name").text(nameInput);
       $("#squirrel").show();
     } else if (personalityInput === "Extroverted" && perspectiveInput === "Optimistic" && teamInput === "On a team"){
+      $(".name").text(nameInput);
       $("#monkey").show();
     } else {
+      $(".name").text(nameInput);
       $("#dog").show();
     }
   });
-
 });
